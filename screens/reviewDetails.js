aaf5267c9adb9cd07362c6871d2b59b591188677
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2023-10-23 01:28:19 +0100

    emplemnt screen for test

---
+++ screens/reviewDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const ReviewDetails = () => {
+  return (
+    <View>
+      <Text>
+        ReviewDetails
+      </Text>
+    </View>
+  )
+}
+
+export default ReviewDetails
